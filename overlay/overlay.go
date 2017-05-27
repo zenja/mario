7a3a4df632f4946cb77190e2ddb27d8ced5bfd1c
Xing, Wang <xwang16@ebay.com>
2017-05-27 10:27:37 +0800

    surprisingly possible same ticks between two frames

--- overlay/overlay.go
+++ overlay/overlay.go
@@ -3,6 +3,8 @@ package overlay
 import (
 	"fmt"
 
+	"log"
+
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/zenja/mario/graphic"
 	"github.com/zenja/mario/vector"
@@ -22,11 +24,17 @@ type FPSOverlay struct {
 
 func (fo *FPSOverlay) Draw(g *graphic.Graphic, ticks uint32) {
 	var fps uint32
+	pos := vector.Pos{50, 50}
+	color := sdl.Color{255, 255, 255, 0}
 	if fo.currentTicks == 0 {
 		fo.currentTicks = ticks
 		return
 	}
+	if ticks-fo.currentTicks <= 0 {
+		log.Panicf("FPSOverlay: strange, ticks (%d) <= fo.currentTicks (%d)", ticks, fo.currentTicks)
+		g.DrawText(fmt.Sprint("FPS: NaN"), pos, color)
+	}
 	fps = 1000 / (ticks - fo.currentTicks)
-	g.DrawText(fmt.Sprintf("FPS: %d", fps), vector.Pos{50, 50}, sdl.Color{255, 255, 255, 0})
+	g.DrawText(fmt.Sprintf("FPS: %d", fps), pos, color)
 	fo.currentTicks = ticks
 }
