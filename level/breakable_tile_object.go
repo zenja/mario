d26d1411a5cab1a5dc9caa66c46c8881d4f7e1c9
Xing, Wang <zenja.wang@gmail.com>
2017-06-03 17:25:07 +0800

    add breakable tile object

---
+++ level/breakable_tile_object.go
@@ -0,0 +1,54 @@
+package level
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/zenja/mario/graphic"
+	"github.com/zenja/mario/vector"
+	"golang.org/x/tools/container/intsets"
+)
+
+// assert that breakableTileObject is hit-able by hero
+var _ heroHittableObject = &breakableTileObject{}
+
+type breakableTileObject struct {
+	mainRes   graphic.Resource
+	pieceRes  graphic.Resource
+	levelRect sdl.Rect
+	zIndex    int
+}
+
+func NewBreakableTileObject(mainRes graphic.Resource, pieceRes graphic.Resource, startPos vector.Pos, zIndex int) Object {
+	return &breakableTileObject{
+		mainRes:   mainRes,
+		pieceRes:  pieceRes,
+		levelRect: sdl.Rect{startPos.X, startPos.Y, graphic.TILE_SIZE, graphic.TILE_SIZE},
+	}
+}
+
+func (bto *breakableTileObject) Draw(g *graphic.Graphic, camPos vector.Pos) {
+	g.DrawResource(bto.mainRes, bto.levelRect, camPos)
+}
+
+func (bto *breakableTileObject) Update(events *intsets.Sparse, ticks uint32, level *Level) {
+	// Do nothing
+}
+
+func (bto *breakableTileObject) GetRect() sdl.Rect {
+	return bto.levelRect
+}
+
+func (bto *breakableTileObject) GetZIndex() int {
+	return bto.zIndex
+}
+
+func (bto *breakableTileObject) hitByHero(hd hitDirection, level *Level, ticks uint32) {
+	// can only be hit from bottom
+	// TODO in the future the direction may need to be decided from input
+	if hd != HIT_FROM_BOTTOM {
+		return
+	}
+
+	tid := GetTileID(vector.Pos{bto.levelRect.X, bto.levelRect.Y}, false, false)
+	// remove object and obstacle
+	level.RemoveObstacleTileObject(tid)
+}
