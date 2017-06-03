2064fd27219bd761daefb82128a0e283e9afe7b3
Xing, Wang <zenja.wang@gmail.com>
2017-06-03 16:09:03 +0800

    better coin effect

--- level/coin_effect.go
+++ level/coin_effect.go
@@ -8,27 +8,43 @@ import (
 
 type coinEffect struct {
 	coinRes    graphic.Resource
+	tileRect   sdl.Rect
 	levelRect  sdl.Rect
+	velocity   vector.Vec2D
 	startTicks uint32
+	lastTicks  uint32
 	finished   bool
 }
 
 func NewCoinEffect(tid vector.TileID, resourceRegistry map[graphic.ResourceID]graphic.Resource, ticks uint32) Effect {
 	coinRes, _ := resourceRegistry[graphic.RESOURCE_TYPE_COIN]
+	tileRect := *GetTileRect(tid)
 	return &coinEffect{
 		coinRes:    coinRes,
-		levelRect:  *GetTileRect(tid),
+		tileRect:   tileRect,
+		levelRect:  tileRect,
+		velocity:   vector.Vec2D{0, -450},
 		startTicks: ticks,
+		lastTicks:  ticks,
 		finished:   false,
 	}
 }
 
 func (ci *coinEffect) UpdateAndDraw(g *graphic.Graphic, camPos vector.Pos, ticks uint32) {
-	if ticks-ci.startTicks > 700 {
+	// speed up
+	ci.velocity.Y -= 50
+
+	velocityStep := CalcVelocityStep(ci.velocity, ticks, ci.lastTicks, nil)
+	ci.tileRect.X += velocityStep.X
+	ci.tileRect.Y += velocityStep.Y
+
+	if ticks-ci.startTicks > 100 {
 		ci.finished = true
 	} else {
-		g.DrawResource(ci.coinRes, ci.levelRect, camPos)
+		g.DrawResource(ci.coinRes, ci.tileRect, camPos)
 	}
+
+	ci.lastTicks = ticks
 }
 
 func (ci *coinEffect) Finished() bool {
